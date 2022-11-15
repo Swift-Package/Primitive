860a970f4010d865b40ac33c826089169b543835
Tim 杨俊艺 <tim_yang@intretech.com>
2022-11-15 13:29:50 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Primitive
+
+A description of this package.
---
+++ Tests/PrimitiveTests/PrimitiveTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Primitive
+
+final class PrimitiveTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Primitive().text, "Hello, World!")
+    }
+}
