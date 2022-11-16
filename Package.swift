ba3ac4a10dd4e4ea37b3b58f9299ed1dde336553
Tim 杨俊艺 <tim_yang@intretech.com>
2022-11-16 16:02:43 +0800

    第三方库Package依赖汇总源包

--- Package.swift
+++ Package.swift
@@ -5,22 +5,83 @@ import PackageDescription
 
 let package = Package(
     name: "Primitive",
+    platforms: [.iOS(.v15), .macOS(.v13), .watchOS(.v9)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Primitive",
-            targets: ["Primitive"]),
+        .library(name: "Primitive", targets: ["Primitive"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/AFNetworking/AFNetworking", branch: "master"),
+        .package(url: "https://github.com/Alamofire/Alamofire", branch: "master"),
+        .package(url: "https://github.com/onevcat/APNGKit", branch: "master"),
+        .package(url: "https://github.com/AudioKit/AudioKit", branch: "main"),                      // 音频合成
+        .package(url: "https://github.com/Swift-Package/BabyBluetooth", branch: "main"),            // 蓝牙库
+        .package(url: "https://github.com/CombineCommunity/CombineCocoa", branch: "main"),
+        .package(url: "https://github.com/CombineCommunity/CombineExt", branch: "main"),
+        .package(url: "https://github.com/Swift-Package/CWLateralSlide", branch: "main"),           // 侧边控制器
+        .package(url: "https://github.com/sindresorhus/Defaults", branch: "main"),
+        .package(url: "https://github.com/dzenbot/DZNEmptyDataSet", branch: "master"),              // 空数据提示
+        .package(url: "https://github.com/ccgus/fmdb", branch: "master"),                           // 数据库
+        .package(url: "https://github.com/HeroTransitions/Hero", branch: "develop"),                // 视图控制器转换效果
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager", branch: "master"),       // 键盘响应管理
+        .package(url: "https://github.com/joeldev/JLRoutes", branch: "master"),                     // 路由
+        .package(url: "https://github.com/onevcat/Kingfisher", branch: "master"),                   // 图片加载
+        .package(url: "https://github.com/Swift-Package/MasonryXCF", branch: "main"),               // 布局
+        .package(url: "https://github.com/jdg/MBProgressHUD", branch: "master"),                    // 加载动画
+        .package(url: "https://github.com/CoderMJLee/MJExtension", branch: "master"),               // 模型转换
+        .package(url: "https://github.com/CoderMJLee/MJRefresh", branch: "master"),                 // 下拉刷新
+        .package(url: "https://github.com/Swift-Package/MQTTClient", branch: "main"),               // MQTT
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView", branch: "master"),    // 加载动画
+        .package(url: "https://github.com/PureLayout/PureLayout", branch: "master"),
+        .package(url: "https://github.com/Swift-Package/ReactiveObjcXCF", branch: "main"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage", branch: "master"),
+        .package(url: "https://github.com/Swift-Package/Skywalker", branch: "main"),
+        .package(url: "https://github.com/SnapKit/SnapKit", branch: "develop"),
+        .package(url: "https://github.com/ivanvorobei/SPIndicator", branch: "main"),
+        .package(url: "https://github.com/yonat/SweeterSwift", branch: "master"),
+        .package(url: "https://github.com/SwifterSwift/SwifterSwift", branch: "master"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", branch: "master"),                // JSON
+        .package(url: "https://github.com/Swift-Package/TZImagePickerControllerXCF", branch: "main"),// 照片选择
+        .package(url: "https://github.com/Swift-Package/YYKitXCF", branch: "main"),                 // YYKit
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Primitive",
-            dependencies: []),
+            dependencies: [
+                .product(name: "AFNetworking", package: "AFNetworking"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "APNGKit", package: "APNGKit"),
+                .product(name: "AudioKit", package: "AudioKit"),
+                .product(name: "BabyBluetooth", package: "BabyBluetooth"),
+                .product(name: "CombineCocoa", package: "CombineCocoa"),
+                .product(name: "CombineExt", package: "CombineExt"),
+                .product(name: "CWLateralSlide", package: "CWLateralSlide"),
+                .product(name: "Defaults", package: "Defaults"),
+                .product(name: "DZNEmptyDataSet", package: "DZNEmptyDataSet"),
+                .product(name: "FMDB", package: "FMDB"),
+                .product(name: "Hero", package: "Hero"),
+                .product(name: "IQKeyboardManagerSwift", package: "IQKeyboardManager"),
+                .product(name: "JLRoutes", package: "JLRoutes"),
+                .product(name: "Kingfisher", package: "Kingfisher"),
+                .product(name: "Masonry", package: "MasonryXCF"),
+                .product(name: "MBProgressHUD", package: "MBProgressHUD"),
+                .product(name: "MJExtension", package: "MJExtension"),
+                .product(name: "MJRefresh", package: "MJRefresh"),
+                .product(name: "MQTTClient", package: "MQTTClient"),
+                .product(name: "NVActivityIndicatorView", package: "NVActivityIndicatorView"),
+                .product(name: "NVActivityIndicatorViewExtended", package: "NVActivityIndicatorView"),
+                .product(name: "PureLayout", package: "PureLayout"),
+                .product(name: "ReactiveObjc", package: "ReactiveObjcXCF"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+                .product(name: "Skywalker", package: "Skywalker"),
+                .product(name: "SkywalkerOC", package: "Skywalker"),
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "SPIndicator", package: "SPIndicator"),
+                .product(name: "SweeterSwift", package: "SweeterSwift"),
+                .product(name: "SwifterSwift", package: "SwifterSwift"),
+                .product(name: "SwiftyJSON", package: "SwiftyJSON"),
+                .product(name: "TZImagePickerController", package: "TZImagePickerControllerXCF"),
+                .product(name: "YYKit", package: "YYKitXCF")
+            ]),
         .testTarget(
             name: "PrimitiveTests",
             dependencies: ["Primitive"]),
--- Sources/Primitive/Primitive.swift
+++ Sources/Primitive/Primitive.swift
@@ -1,6 +1,39 @@
+import AFNetworking
+import Alamofire
+import APNGKit
+import AudioKit
+import BabyBluetooth
+import CombineCocoa
+import CombineExt
+import CWLateralSlide
+import Defaults
+import DZNEmptyDataSet
+import FMDB
+import IQKeyboardManagerSwift
+import JLRoutes
+import Kingfisher
+import Masonry
+import MBProgressHUD
+import MJExtension
+import MJRefresh
+import MQTTClient
+import NVActivityIndicatorView
+import NVActivityIndicatorViewExtended
+import PureLayout
+import ReactiveObjC
+import SDWebImage
+import Skywalker
+import SkywalkerOC
+import SnapKit
+import SPIndicator
+import SweeterSwift
+import SwifterSwift
+import SwiftyJSON
+import TZImagePickerController
+import YYKit
+
 public struct Primitive {
     public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+    
+    public init() {}
 }
